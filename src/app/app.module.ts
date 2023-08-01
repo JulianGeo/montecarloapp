8a2e03756a3cccece46c0bf2eba2c278e0fab94b
Julian Rojas <julianrojasgeo@gmail.com>
2023-08-01 18:32:56 -0500

    Main components and pages files were created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninFormComponent } from './components/forms/signin-form/signin-form.component';
+import { SignupFormComponent } from './components/forms/signup-form/signup-form.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SimulatorComponent } from './pages/simulator/simulator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninFormComponent,
+    SignupFormComponent,
+    AboutComponent,
+    AuthComponent,
+    NotFoundComponent,
+    SimulatorComponent
   ],
   imports: [
     BrowserModule,
