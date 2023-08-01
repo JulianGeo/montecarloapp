e5c2beabb97c4fa5a3d8bf856344d6a78aeca402
Julian Rojas <julianrojasgeo@gmail.com>
2023-08-01 18:38:48 -0500

    Main pages were linked in the router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SimulatorComponent } from './pages/simulator/simulator.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+
+  {
+    path: 'home',
+    component: HomePageComponent,
+  },
+  {
+    path: 'aboutme',
+    component: AboutComponent
+  },
+  {
+    path: 'simulator',
+    component: SimulatorComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
