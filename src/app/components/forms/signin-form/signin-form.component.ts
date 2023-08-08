686143bc6572f383dc0eb13ff68dd1e4b61406cf
Julian Rojas <julianrojasgeo@gmail.com>
2023-08-08 16:12:48 -0500

    Sign in form was created

--- src/app/components/forms/signin-form/signin-form.component.ts
+++ src/app/components/forms/signin-form/signin-form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SignInService } from 'src/app/services/sign-in.service';
 
 @Component({
   selector: 'app-signin-form',
@@ -6,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./signin-form.component.scss']
 })
 export class SigninFormComponent {
+
+  formLogin: FormGroup;
+
+  constructor(
+    private signInService: SignInService,
+    private router: Router
+  ) {
+    this.formLogin = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null)
+    })
+  }
+
+
+  onSubmit(): void {
+    this.signInService.signin(this.formLogin.value)
+    .subscribe({
+      next: (user) => {
+        console.log(user)
+      },
+      error: (console.log),
+      complete: (console.log)
+    })
+
+
+
+
+/*     this.userService.login(this.formLogin.value)
+      .then(response => {
+        this.fetchUserData();
+        this.router.navigate(['/main'])
+      })
+      .catch(error => console.log(error)); */
+      console.log("submit")
+  }
 
 }
