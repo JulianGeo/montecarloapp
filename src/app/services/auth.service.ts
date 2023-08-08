315f50f95e1b09d886d2dfc40ead73f911ac67bf
Julian Rojas <julianrojasgeo@gmail.com>
2023-08-08 18:47:00 -0500

    Auth methods included in auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { UserLogin } from '../models/user-login';
+import { LocalStorageService } from './local-storage.service';
 
 const URL_BASE = "http://localhost:8080/api/1.0/user/signin"
 
@@ -10,9 +11,39 @@ const URL_BASE = "http://localhost:8080/api/1.0/user/signin"
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private localStorage: LocalStorageService
+    ) { }
 
   signin(user: UserLogin): Observable<any> {
-    return this.http.post(URL_BASE, user);
+    return this.http.post(URL_BASE, user).pipe(
+      map((userInfo: any) => {
+        this.localStorage.setToken(userInfo.token);
+        return "User is logged in"
+      })
+    );
   }
+
+  isLoggedIn(): boolean {
+    const token = this.localStorage.getToken();
+
+    if (token) {
+      const decodedToken = this.decodeToken(token);
+      const now = Date.now() / 1000;
+
+      return decodedToken.exp && decodedToken.exp > now;
+    }
+    return false;
+  }
+
+  signOut(){
+    this.localStorage.removeToken();
+  }
+
+  private decodeToken(token: string): any {
+    const payload = token.split('.')[1];
+    return JSON.parse(atob(payload));
+  }
+
 }
