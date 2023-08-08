519578b305aa788817c288f4341f8598dbbfb891
Julian Rojas <julianrojasgeo@gmail.com>
2023-08-08 16:11:12 -0500

    Auth services were implemented

--- src/app/services/sign-in.service.ts
+++ src/app/services/sign-in.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UserLogin } from '../models/user-login';
+import { Observable } from 'rxjs';
+
+
+const URL_BASE = "http://localhost:8080/api/1.0/user/signin"
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignInService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  signin(user: UserLogin): Observable<any> {
+    return this.http.post(URL_BASE, user);
+  }
 }
--- src/app/services/sign-up.service.ts
+++ src/app/services/sign-up.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { Observable } from 'rxjs';
+
+const URL_BASE = "http://localhost:8080/api/1.0/user/signup"
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignUpService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  signup(user: User): Observable<any> {
+    return this.http.post(URL_BASE, user);
+  }
 }
